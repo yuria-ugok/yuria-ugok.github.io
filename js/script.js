555c5a6675329a6322ec5ddcfd455bf6717be4c3
Yuria <77422816+yuria-ugok@users.noreply.github.com>
2021-03-03 20:33:08 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+jQuery(function() {
+    var pagetop = $('#page_top');   
+    pagetop.hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 500) {  //500pxスクロールしたら表示
+            pagetop.fadeIn();
+        } else {
+            pagetop.fadeOut();
+        }
+    });
+  $('a[href^="#"]').click(function(){
+    var time = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" ? 'html' : href);
+    var distance = target.offset().top;
+    $("html, body").animate({scrollTop:distance}, time, "swing");
+    return false;
+  });
+});
